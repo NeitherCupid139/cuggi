94a3e28af3f72eb42fd2237114f55583e1e49c58
NeitherCupid139 <2694674113@qq.com>
2023-12-30 16:33:50 +0800

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),UnoCSS(),],
+  plugins: [vue(),UnoCSS(),
+    VitePWA({ registerType: 'autoUpdate',injectRegister: 'auto',
+    manifest: {
+      name: 'word=>world',
+      short_name: 'word=>world',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/src/assets/favicon128.png',
+          sizes: '128x128',
+          type: 'image/png',
+        },
+        {
+          src: 'src/assets/favicon512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
 })
 
 
