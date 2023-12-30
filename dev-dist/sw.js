c59d98c228d63eb5c1083e519b6d72f7a4bb361d
NeitherCupid139 <2694674113@qq.com>
2023-12-30 17:00:41 +0800

    pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e6323a81'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9j384ab1oio"
+    "revision": "0.s2hrtcgibb"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
