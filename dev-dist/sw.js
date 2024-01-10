689604729c951b85b8e6119a2b15b48dc7d058d6
NeitherCupid139 <2694674113@qq.com>
2024-01-10 12:50:35 +0800

    disable scale

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e6323a81'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eoj406ure4g"
+    "revision": "0.ebv599klci8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
